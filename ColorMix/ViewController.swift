15e3a865b5bf8cb0281506d6ccf2877a156b3e0a
Adam Goga <adam.goga21@gmail.com>
2020-01-31 12:00:54 -0500

    Added Color View
    constant, aqua color

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var colorView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let color = UIColor(red: 0.1, green: 0.45, blue: 0.45, alpha: 1)
+        colorView.backgroundColor = color
         // Do any additional setup after loading the view.
     }
 
