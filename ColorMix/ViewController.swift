70e29bc88ce6e4ceb1652300a120762fbe4613b9
Adam Goga <adam.goga21@gmail.com>
2020-02-03 12:00:02 -0500

    Toggling Color
    between aqua and marooon with a switch

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -12,13 +12,21 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var colorView: UIView!
 
+    let onColor = UIColor(red: 0.1, green: 0.45, blue: 0.45, alpha: 1)
+    let offColor = UIColor(red: 0.5, green: 0.1, blue: 0.2, alpha: 1)
     override func viewDidLoad() {
         super.viewDidLoad()
-        let color = UIColor(red: 0.1, green: 0.45, blue: 0.45, alpha: 1)
-        colorView.backgroundColor = color
+        colorView.backgroundColor = onColor
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            colorView.backgroundColor = onColor
+        } else {
+            colorView.backgroundColor = offColor
+        }
+    }
+    
 }
 
