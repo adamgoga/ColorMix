9946bb4421386c1092789c4f771e69c03f324c87
Adam Goga <adam.goga21@gmail.com>
2020-02-13 12:00:12 -0500

    Percent Label
    Made a percentage label for how much color there was, set to 0 if switch was off.

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -11,35 +11,52 @@ import UIKit
 class ViewController: UIViewController {
    
     @IBOutlet weak var redSwitch: UISwitch!
-    @IBOutlet weak var greenSwtich: UISwitch!
-    @IBOutlet weak var blueSwtich: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setColor()
+        setText()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
         setColor()
+        setText()
     }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
         setColor()
+        setText()
     }
     
     func setColor() {
         let red: CGFloat = redSwitch.isOn ? CGFloat(redSlider.value) : 0
-        let green: CGFloat = greenSwtich.isOn ? CGFloat(greenSlider.value) : 0
-        let blue: CGFloat = blueSwtich.isOn ? CGFloat(blueSlider.value) : 0
+        let green: CGFloat = greenSwitch.isOn ? CGFloat(greenSlider.value) : 0
+        let blue: CGFloat = blueSwitch.isOn ? CGFloat(blueSlider.value) : 0
         
         let color = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
         self.view.backgroundColor = color
         
+    }
+    
+    func setText() {
+        let red = redSwitch.isOn ? Int(redSlider.value * 100) : 0
+        let green = greenSwitch.isOn ? Int(greenSlider.value * 100) : 0
+        let blue = blueSwitch.isOn ?  Int(blueSlider.value * 100) : 0
+        
+        redLabel.text = "Red \(red)%"
+        greenLabel.text = "Green \(green)%"
+        blueLabel.text = "Blue \(blue)%"
     }
 }
 
